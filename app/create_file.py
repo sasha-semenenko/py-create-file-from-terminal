f07c07584c5601323141df873fdca1e244c0e9fe
Sasha Semenenko <semenenko233249@gmail.com>
2024-01-26 20:27:52 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,37 +3,54 @@ import os
 from datetime import datetime
 
 
-def create_file() -> None:
-    command = sys.argv
+def get_directory(command: list) -> str:
+    if "-d" in command and "-f" in command:
+        flag_d = command.index("-d")
+        flag_f = command.index("-f")
+        if flag_d < flag_f:
+            path = os.path.join(*command[(flag_d + 1):flag_f])
+            os.makedirs(path, exist_ok=True)
+            file_name = command[flag_f + 1]
+        else:
+            path = os.path.join(*command[(flag_d + 1):])
+            os.makedirs(path, exist_ok=True)
+            file_name = command[flag_f + 1]
+        return os.path.join(path, file_name)
 
     if "-d" in command:
-        current_directory = command[command.index("-d") + 1]
+        flag = command.index("-d")
         path = os.path.join(
-            current_directory, command[command.index("-d") + 2]
+            *command[(flag + 1):]
         )
         os.makedirs(path, exist_ok=True)
 
     if "-f" in command:
-        if "-d" in command:
-            current_directory = command[command.index("-d") + 1]
-            new_directory = command[command.index("-d") + 2]
-            file_name = command[command.index("-f") + 1]
-            file_path = os.path.join(
-                current_directory, new_directory, file_name
-            )
-        else:
-            file_path = command[command.index("-f") + 1]
+        flag_f = command.index("-f")
+        file_name = command[flag_f + 1]
 
-        with open(file_path, "a") as file:
-            file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S \n"))
-            count_line = 0
-            while True:
-                enter_line = input("Enter content line: ")
-                count_line += 1
-                if enter_line == "stop":
-                    break
-                file.write(f"{count_line} {enter_line}\n")
-            file.write("\n")
+        return file_name
+
+
+def create_and_write_file(file_path: str) -> None:
+    with open(f"{file_path}", "a") as file:
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S \n"))
+        count_line = 0
+        while True:
+            enter_line = input("Enter content line: ")
+            count_line += 1
+            if enter_line == "stop":
+                break
+            file.write(f"{count_line} {enter_line}\n")
+        file.write("\n")
+
+
+def create_file() -> None:
+    commands = sys.argv
+
+    file_path = get_directory(commands)
+
+    if file_path is not None:
+        create_and_write_file(file_path)
 
 
 create_file()
